82892b7781933eebdb8702e1f3a9256391731e41
design-indra <ghostersoloist@gmail.com>
2026-05-08 11:36:41 +0700

    fix: update semua perubahan

--- app/lokasi/page.tsx
+++ app/lokasi/page.tsx
@@ -50,9 +50,9 @@ export default function LokasiPage() {
             <div>
               <p className="font-semibold text-sm">Alamat</p>
               <p className="text-sm text-slate-500 dark:text-slate-400 mt-1">
-                Jl. Contoh No. 123<br />
+                Athifa Battery<br />
                 Jl. Raya Bojonegara, Kp. Gedong, Kec. Bojonegara, Kab. Serang, Banten 42161<br />
-                <em className="text-xs">(Update sesuai alamat asli)</em>
+                <em className="text-xs">(lokasi toko)</em>
               </p>
             </div>
           </div>
