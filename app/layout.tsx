1b708f08e7da4910470c12fda5a42ac02723f82f
design-indra <ghostersoloist@gmail.com>
2026-05-11 09:18:11 +0700

    feat: pasang Google Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -54,6 +54,13 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="id" suppressHydrationWarning>
       <head>
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-3BBB4B1408"></script>
+<script dangerouslySetInnerHTML={{ __html: `
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'G-3BBB4B1408');
+`}} />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
